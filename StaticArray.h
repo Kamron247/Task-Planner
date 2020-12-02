0643b4fcf1606b20061098bb35f7d0ea86db8514
Kamron <45323286+slowkambo@users.noreply.github.com>
2020-12-02 15:41:46 -0800

    Update StaticArray.h

--- StaticArray.h
+++ StaticArray.h
@@ -1,51 +1,51 @@
 #pragma once
 // Programmer: Ziv Fuchs
-// Programmer’s ID: 1796140
+// Programmerï¿½s ID: 1796140
 
 #pragma once
 
 template <typename T, int CAP>
 class StaticArray
 {
-  T value[CAP];
-  T dummy;
+    T value[CAP];
+    T dummy;
 
 public:
-  StaticArray();
-  int capacity() const;
-  T operator[](int) const;
-  T& operator[](int);
+    StaticArray();
+    int capacity() const;
+    T operator[](int) const;
+    T& operator[](int);
 };
 
 template <typename T, int CAP>
-int StaticArray<T, CAP>::capacity() const 
+int StaticArray<T, CAP>::capacity() const
 {
-  return CAP;
+    return CAP;
 }
 
 template <typename T, int CAP>
 StaticArray<T, CAP>::StaticArray()
 {
-  for (int i = 0; i < CAP; i++)
-  {
-    value[i] = T();
-  }
+    for (int i = 0; i < CAP; i++)
+    {
+        value[i] = T();
+    }
 }
 
 template <typename T, int CAP>
 T StaticArray<T, CAP>::operator[](int index) const
 {
-  if ((index < 0) || (index >= CAP))
-    return dummy;
+    if ((index < 0) || (index >= CAP))
+        return dummy;
 
-  return value[index];
+    return value[index];
 }
 
 template <typename T, int CAP>
 T& StaticArray<T, CAP>::operator[](int index)
 {
-  if ((index < 0) || (index >= CAP))
-    return dummy;
+    if ((index < 0) || (index >= CAP))
+        return dummy;
 
-  return value[index];
+    return value[index];
 }
